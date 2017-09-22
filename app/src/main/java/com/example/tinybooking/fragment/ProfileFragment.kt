7791c8727d582d46577fc7fe03d4a2222fb0245d
schecterza <phuengton.ch.57@ubu.ac.th>
2017-09-22 13:06:41 +0700

    add login

--- app/src/main/java/com/example/tinybooking/fragment/ProfileFragment.kt
+++ app/src/main/java/com/example/tinybooking/fragment/ProfileFragment.kt
@@ -5,7 +5,12 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
 import com.example.tinybooking.R
+import com.example.tinybooking.dao.UserData
+import kotlinx.android.synthetic.main.fragment_profile.view.*
 
 /**
  * Created by schecterza on 9/12/2017 AD.
@@ -13,6 +18,9 @@ import com.example.tinybooking.R
 
 class ProfileFragment: Fragment() {
 
+    lateinit var btnLogin: LinearLayout
+
+    lateinit var textViewUserName: TextView
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rootView = inflater!!.inflate(R.layout.fragment_profile, container, false)
@@ -22,7 +30,29 @@ class ProfileFragment: Fragment() {
 
     fun initInstances(rootView: View) {
 
+        btnLogin = rootView.btn_log_in
+        btnLogin.setOnClickListener(myOnClick)
+
+        textViewUserName = rootView.textView_user_name
+
+        val userData = UserData.getUserData()
+        Toast.makeText(activity, userData.userDisplayName, Toast.LENGTH_LONG).show()
+
     }
+
+    val myOnClick = View.OnClickListener{v ->
+        when(v) {
+            v -> goToSignInView()
+        }
+    }
+
+    fun goToSignInView() {
+        fragmentManager.beginTransaction()
+                .replace(R.id.content_main_activity, SignInFragment.newInstance())
+                .addToBackStack(null)
+                .commit()
+    }
+
 
     companion object {
         fun newInstance(): ProfileFragment {

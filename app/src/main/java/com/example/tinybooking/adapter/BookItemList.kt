2ac8bb4822ebc41d25e250cb68b2497abc497f31
weiweieuroo <korawit.in.57@ubu.ac.th>
2017-09-23 12:07:12 +0700

    ..

---
+++ app/src/main/java/com/example/tinybooking/adapter/BookItemList.kt
@@ -0,0 +1,33 @@
+package com.example.tinybooking.adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.tinybooking.R
+
+/**
+ * Created by schecterza on 9/22/2017.
+ */
+
+class BookItemList: RecyclerView.Adapter<BookItemList.ViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
+        var view = LayoutInflater.from(parent!!.context).inflate(R.layout.cardview_book, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int = 5
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        fun binData() {
+
+        }
+    }
+}
--- app/src/main/java/com/example/tinybooking/fragment/BookFragment.kt
+++ app/src/main/java/com/example/tinybooking/fragment/BookFragment.kt
@@ -2,17 +2,29 @@ package com.example.tinybooking.fragment
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.LinearSnapHelper
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.SnapHelper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import com.example.tinybooking.R
+import com.example.tinybooking.adapter.BookItemList
+import com.example.tinybooking.adapter.FavoriteItemListAdapter
+import kotlinx.android.synthetic.main.fragment_book.view.*
+import kotlinx.android.synthetic.main.fragment_main.view.*
 
 /**
  * Created by schecterza on 9/12/2017 AD.
  */
 
 class BookFragment : Fragment() {
-
+    private var mSnapHelper: SnapHelper? = null
+    private var mRecyclerView: RecyclerView? = null
+    private var mAdapter: RecyclerView.Adapter<*>? = null
+    private var mLayoutManager: RecyclerView.LayoutManager? = null
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         var rootView = inflater!!.inflate(R.layout.fragment_book, container, false)
         initInstances(rootView)
@@ -20,7 +32,17 @@ class BookFragment : Fragment() {
     }
 
     fun initInstances(rootView: View) {
+        mRecyclerView = rootView.list_book
+        mRecyclerView!!.setHasFixedSize(true)
 
+        mLayoutManager = LinearLayoutManager(context, LinearLayout.HORIZONTAL, false)
+        mRecyclerView!!.layoutManager = mLayoutManager
+
+        mAdapter = BookItemList()
+        mRecyclerView!!.adapter = mAdapter
+
+        mSnapHelper = LinearSnapHelper()
+        mSnapHelper!!.attachToRecyclerView(mRecyclerView)
     }
 
     companion object {

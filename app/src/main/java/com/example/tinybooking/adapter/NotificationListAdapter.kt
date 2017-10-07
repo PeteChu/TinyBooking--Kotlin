3a975d8c06a5f9f6bce88b7ba569f508e1d8db4e
weiweieuroo <korawit.in.57@ubu.ac.th>
2017-10-08 00:56:36 +0700

    ...

--- app/src/main/java/com/example/tinybooking/adapter/NotificationListAdapter.kt
+++ app/src/main/java/com/example/tinybooking/adapter/NotificationListAdapter.kt
@@ -9,7 +9,9 @@ import android.view.ViewGroup
 import android.widget.Toast
 import com.example.tinybooking.NotidetailActivity
 import com.example.tinybooking.R
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.item_notification.view.*
+
 
 
 /**
@@ -36,14 +38,37 @@ class NotificationListAdapter() : RecyclerView.Adapter<NotificationListAdapter.V
     }
 
     override fun getItemCount(): Int {
-        return 7
+        return 3
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val card = itemView.item_noti
+        val imageItem = itemView.noti_item_image
+        val title = itemView.noti_title
+        val subtitle = itemView.noti_subtitle
+        lateinit var urlImage : String
+        lateinit var tmpTitle : String
         fun bind(mContext:Context,position: Int){
+
+            if(position==0){
+                 urlImage = "https://www.mx7.com/i/0ee/RYTz3Z.jpg"
+                tmpTitle = "Limited offering for members only!"
+                subtitle.text = "On September 25"
+            }else if(position==1){
+                 urlImage = "https://www.mx7.com/i/296/WqUmpb.jpg"
+                tmpTitle = "Dao Stadium Get 20% off any time."
+                subtitle.text = "20% OFF"
+            }else{
+                urlImage = "https://www.mx7.com/i/1be/CePQzr.jpg"
+                tmpTitle = "Welcome to our new stadium!"
+                subtitle.text = "We’re now ready for serving you at Gaysorn Tower"
+            }
+            Picasso.with(mContext).load(urlImage).into(imageItem)
+            title.text = tmpTitle
             card.setOnClickListener(View.OnClickListener {
                 var intent = Intent(mContext, NotidetailActivity::class.java)
+                intent.putExtra("url",urlImage)
+                intent.putExtra("title",tmpTitle)
                 mContext.startActivity(intent)
             })
         }
